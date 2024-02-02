769fd5838917f6b47eaa7399e44240089e01c29a
ANJANA A <81566311+Anjana2002@users.noreply.github.com>
2024-02-02 23:36:28 +0530

    Update maximize.py

--- sct/maximize.py
+++ sct/maximize.py
@@ -1,24 +1,37 @@
+# Write a program for Genetic algorithm to maximize the function f(x)=x2.
 import numpy as np
 def objective_function(x):
-    return -x**2 + 4*x - 4
+    return x**2
 
-population_size=50
+population_size = 100
 generations = 100
 mutation_rate = 0.1
-                                                                                                                                        
-population  = np.random.uniform(-10,10 , size=population_size)
+
+#initialization
+population = np.random.uniform(-10,10, size = population_size)
+
 for generation in range(generations):
+    # Evaluate the fitness of each individual in the population
     fitness = objective_function(population)
-    sorted_indicies = np.argsort(fitness)[::-1]
-    selected_pop = population[sorted_indicies[:population_size]]
 
-    crossover_pop = np.random.choice(selected_pop, size=population_size)
-    mutation_mask = np.random.rand(population_size)<mutation_rate
-    mutation_pop = np.random.uniform(-1,1,size=population_size)
-    crossover_pop[mutation_mask]+=mutation_pop[mutation_mask]
+    # Select the top individuals based on fitness
+    sorted_indices = np.argsort(fitness) [::-1]
+    selected_pop = population[sorted_indices[:population_size]]
 
-    population= crossover_pop
+    # Crossover: Create new individuals by combining pairs of selected individuals
+    crossover = np.random.choice(selected_pop, size= population_size)
+
+    # Mutation: Introduce random changes to some individuals
+    mutation_mask = np.random.rand(population_size) < mutation_rate
+    mutation_pop = np.random.uniform(-1, 1, size = population_size)
+    crossover[mutation_mask] +=mutation_pop[mutation_mask]
+
+    # Replace the old population with the new one
+    population = crossover
+
+# Find the best individual in the final population
 best_individual = np.argmax(objective_function(population))
-best_x=population[best_individual]
-print(best_x)
-print(objective_function(best_x))
+best_x = population[best_individual]
+
+print(f"Optimal x: {best_x}")
+print(f"Optimal f(x): {objective_function(best_x)}")
